326dcdd0fb1c7a3de2e05b5952e0fb32e897bfd7
muhammedshamil8 <muhammedshamil008@gmail.com>
2023-11-14 22:41:33 +0530

    #

--- react/src/pages/contactUs.jsx
+++ react/src/pages/contactUs.jsx
@@ -1,9 +1,25 @@
+// ContactUs.js
+
+import React from 'react';
+import './contactUsStyles.css';
 
 export default function ContactUs() {
-    return (
-      <div>
-          Contact Us page
-      </div>
-    );
-  }
-  
+  return (
+    <div className="contact-us-container">
+      <h1>Contact Us</h1>
+      <p>Feel free to reach out to us!</p>
+      <form className="contact-form">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" name="message" rows="4"></textarea>
+
+        <button onClick={alert('cooming soon!')}>Submit</button>
+      </form>
+    </div>
+  );
+}
