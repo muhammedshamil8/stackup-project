76f96c6a79e3a11907bf4e577bdd4eeb14f26e35
muhammedshamil8 <muhammedshamil008@gmail.com>
2023-11-15 00:56:08 +0530

    #web app checking

--- react/src/pages/contactUs.jsx
+++ react/src/pages/contactUs.jsx
@@ -9,16 +9,16 @@ export default function ContactUs() {
       <h1>Contact Us</h1>
       <p>Feel free to reach out to us!</p>
       <div className="contact-form">
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" />
+        <label htmlFor="name" className='contact-label'>Name:</label>
+        <input type="text" id="name" name="name" className='contact-input'/>
 
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" name="email"  className='contact-input'/>
 
         <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" rows="4"></textarea>
+        <textarea id="message" name="message" rows="4"  className='contact-input'></textarea>
 
-        <button onClick={alert('cooming soon!')}>Submit</button>
+        <button onClick={alert('cooming soon!')} className='contact-btn'>Submit</button>
       </div>
     </div>
   );
