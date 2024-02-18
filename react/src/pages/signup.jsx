88cc8f274c5739598b2aea5bb2ea2689317f485f
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-02-18 20:55:08 +0530

    #

--- react/src/pages/signup.jsx
+++ react/src/pages/signup.jsx
@@ -5,12 +5,12 @@ import axiosClient from "../axiosClient";
 
 
 function Signup() {
-   
+
     const Navigate = useNavigate();
     const [isFormVisible, setFormVisible] = useState(true);
     const [inputs, setInputs] = useState({});
     const [errors, setErrors] = useState({});
-    
+
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
@@ -19,75 +19,94 @@ function Signup() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        setErrors({}); 
+        setErrors({});
 
         axiosClient.post(`/signup.php`, inputs).then(function (response) {
+            console.log(response.data)
             if (response.data.status === 0) {
                 setErrors({ message: response.data.message });
-            } else if (response.data.status === 1){
-                Navigate('/login');
-            }else {
+            } else if (response.data.status === 1) {
+                axiosClient
+                    .post("/login.php", inputs)
+                    .then(function (response) {
+
+                        if (response.data.status === 1) {
+                            // Successful login, redirect to a dashboard or home page
+                            // Store the user ID in localStorage
+                            localStorage.setItem('userId', response.data.user_id);
+                            Navigate(`/dashboard`);
+
+
+                        } else {
+                            console.log('working');
+                        }
+                    })
+                    .catch(function (error) {
+                        console.error("Error: " + error);
+                        // alert('An error occurred. Please try again later. i think backend server is down')
+                    });
+            } else {
                 setErrors({ message: "An error occurred. Please try again later." });
             }
         })
-        .catch(function (error) {
-            console.error("Error: " + error);
-            // alert('An error occurred. Please try again later. i think backend server is down')
-        });
+            .catch(function (error) {
+                console.error("Error: " + error);
+                // alert('An error occurred. Please try again later. i think backend server is down')
+            });
     }
 
     const handleLinkClick = () => {
-        setFormVisible(false); 
+        setFormVisible(false);
         setTimeout(() => {
             Navigate('/login');
-        }, 700); 
+        }, 700);
     };
 
     return (
         <div className={`login-signup-form animated ${isFormVisible ? 'fadeInDown' : 'slideOut'}`}>
 
-        <div className="form ">
-            <form onSubmit={handleSubmit}>
-                <h1 className="title">Sign up for free</h1>
-                {errors.message && <div className="error">{errors.message}</div>}
+            <div className="form ">
+                <form onSubmit={handleSubmit}>
+                    <h1 className="title">Sign up for free</h1>
+                    {errors.message && <div className="error">{errors.message}</div>}
 
-                <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    placeholder="Full name"
-                    onChange={handleChange}
-                    className="input"
-                />
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Email"
-                    onChange={handleChange}
-                />
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Password"
-                    onChange={handleChange}
-                    autoComplete="new-password"
-                />
-                <input
-                    type="password"
-                    id="passwordConfirm"
-                    name="passwordConfirm"
-                    placeholder="Confirm Password"
-                    onChange={handleChange}
-                    autoComplete="new-password"
-                />
-                <button className="btn btn-block">Signup</button>
-                <p className="message">
-                    Already Registered? <a className="link"  onClick={handleLinkClick}>Sign in</a>
-                </p>
-            </form>
-        </div>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Full name"
+                        onChange={handleChange}
+                        className="input"
+                    />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Email"
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={handleChange}
+                        autoComplete="new-password"
+                    />
+                    <input
+                        type="password"
+                        id="passwordConfirm"
+                        name="passwordConfirm"
+                        placeholder="Confirm Password"
+                        onChange={handleChange}
+                        autoComplete="new-password"
+                    />
+                    <button className="btn btn-block" onClick={handleSubmit}>Signup</button>
+                    <p className="message">
+                        Already Registered? <a className="link" onClick={handleLinkClick}>Sign in</a>
+                    </p>
+                </form>
+            </div>
         </div>
     );
 }
