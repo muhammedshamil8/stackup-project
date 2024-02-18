610d018bdd0bc0930fdac746c25db60dc1487521
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-02-18 20:28:45 +0530

    hosting

--- react/src/axiosClient.js
+++ react/src/axiosClient.js
@@ -2,10 +2,14 @@ import axios from "axios";
 
 
 const axiosClient = axios.create({
-  baseURL: 'https://featuresphere.wuaze.com/',
-})
-// for development ...! or local run  baseURL: 'http://localhost:9000/api',
+
 // for hosting domain ...!
-    //     baseURL: 'https://task-managment-app.k.strikerlulu.me',
+          //  baseURL: 'https://featuresphere.wuaze.com/',
+// for development ...! or local run  
+        // baseURL: 'http://localhost:9000/',
+// for hosting domain ...!
+        baseURL: 'https://task.k.strikerlulu.me',
+})
+
   
 export default axiosClient;
